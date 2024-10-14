2cd43038fbbed128693f0926687fda0dea2a51bf
Martin Bens <martin.bens@it-bens.de>
2024-10-14 11:48:14 +0200

    Test for prioritization of `DefinitionCollectionPopulator` implementation injection into `DefinitionProvider` added.

--- tests/Functional/Definition/DefinitionProviderTest.php
+++ tests/Functional/Definition/DefinitionProviderTest.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace ITB\ShopwareSdkBundle\Tests\Functional\Definition;
 
 use ITB\ShopwareSdkBundle\DependencyInjection\Configuration;
-use ITB\ShopwareSdkBundle\Tests\Functional\Definition\DefinitionProviderTest\DefinitionProviderTestKernel;
+use ITB\ShopwareSdkBundle\Tests\ITBShopwareSdkBundleKernel;
+use ITB\ShopwareSdkBundle\Tests\Mock\AdditionalDefinitionCollectionPopulator;
+use ITB\ShopwareSdkBundle\Tests\Mock\AdditionalDefinitionCollectionPopulatorWithException;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\Yaml\Yaml;
+use Vin\ShopwareSdk\Definition\DefinitionCollection;
+use Vin\ShopwareSdk\Definition\DefinitionProvider;
 use Vin\ShopwareSdk\Definition\DefinitionProviderInterface;
 
 /**
@@ -16,20 +21,40 @@ use Vin\ShopwareSdk\Definition\DefinitionProviderInterface;
  */
 final class DefinitionProviderTest extends TestCase
 {
-    public static function provider(): \Generator
+    public static function withAdditionalDefinitionCollectionPopulatorWithExceptionProvider(): \Generator
     {
         $config = Yaml::parseFile(__DIR__ . '/../../Fixtures/Configuration/config_with_enabled_cache.yaml');
 
-        yield [$config];
+        $additionalDefinitionCollectionPopulatorDefinition = new Definition(AdditionalDefinitionCollectionPopulatorWithException::class);
+        $additionalDefinitionCollectionPopulatorDefinition->setPublic(false);
+
+        yield [
+            $config, [
+                AdditionalDefinitionCollectionPopulatorWithException::class => $additionalDefinitionCollectionPopulatorDefinition,
+            ]];
+    }
+
+    public static function withAdditionalDefinitionCollectionPopulatorProvider(): \Generator
+    {
+        $config = Yaml::parseFile(__DIR__ . '/../../Fixtures/Configuration/config_with_enabled_cache.yaml');
+
+        $additionalDefinitionCollectionPopulatorDefinition = new Definition(AdditionalDefinitionCollectionPopulator::class);
+        $additionalDefinitionCollectionPopulatorDefinition->setPublic(false);
+
+        yield [
+            $config, [
+                AdditionalDefinitionCollectionPopulator::class => $additionalDefinitionCollectionPopulatorDefinition,
+            ]];
     }
 
     /**
      * @param ITBShopwareSdkConfiguration $config
+     * @param array<string, Definition> $dependencyInjectionDefinitions
      */
-    #[DataProvider('provider')]
-    public function testWithAdditionalDefinitionCollectionPopulator(array $config): void
+    #[DataProvider('withAdditionalDefinitionCollectionPopulatorWithExceptionProvider')]
+    public function testWithAdditionalDefinitionCollectionPopulator(array $config, array $dependencyInjectionDefinitions): void
     {
-        $kernel = new DefinitionProviderTestKernel('test', true, $config);
+        $kernel = new ITBShopwareSdkBundleKernel('test', true, $config, $dependencyInjectionDefinitions);
         $kernel->boot();
 
         $container = $kernel->getContainer();
@@ -38,5 +63,35 @@ final class DefinitionProviderTest extends TestCase
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage('This exception is expected.');
         $container->get(DefinitionProviderInterface::class);
+    }
+
+    /**
+     * @param ITBShopwareSdkConfiguration $config
+     * @param array<string, Definition> $dependencyInjectionDefinitions
+     */
+    #[DataProvider('withAdditionalDefinitionCollectionPopulatorProvider')]
+    public function testPriorityWithAdditionalCollectionPopulator(array $config, array $dependencyInjectionDefinitions): void
+    {
+        $kernel = new ITBShopwareSdkBundleKernel('test', true, $config, $dependencyInjectionDefinitions);
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+        $definitionProvider = $container->get(DefinitionProviderInterface::class);
+        /** @var DefinitionProvider $definitionProvider */
+
+        $definitionProviderReflection = new \ReflectionClass($definitionProvider);
+        $definitionCollectionProperty = $definitionProviderReflection->getProperty('definitionCollection');
+        /** @var DefinitionCollection $definitionCollection */
+        $definitionCollection = $definitionCollectionProperty->getValue($definitionProvider);
+
+        $definitionCollectionReflection = new \ReflectionClass($definitionCollection);
+        $definitionsProperty = $definitionCollectionReflection->getProperty('definitions');
+        /** @var array<string, Definition> $definitions */
+        $definitions = $definitionsProperty->getValue($definitionCollection);
+
+        $entityNames = array_keys($definitions);
+        $lastEntityName = $entityNames[count($entityNames) - 1];
+
+        $this->assertSame('new_entity', $lastEntityName);
     }
 }
