60d6154b13b41b020d9c71b02163c57c87fc78e5
Martin Bens <martin.bens@it-bens.de>
2024-10-14 12:14:27 +0200

    Compiler pass added to make all entity repositories autowireable.

--- src/ITBShopwareSdkBundle.php
+++ src/ITBShopwareSdkBundle.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace ITB\ShopwareSdkBundle;
 
 use ITB\ShopwareSdkBundle\DependencyInjection\Compiler\EntityDefinitionCollectionPopulatorsCompilerPass;
+use ITB\ShopwareSdkBundle\DependencyInjection\Compiler\MakeEntityRepositoriesAutowirableCompilerPass;
 use ITB\ShopwareSdkBundle\DependencyInjection\ITBShopwareSdkExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -13,7 +14,10 @@ final class ITBShopwareSdkBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        $container->addCompilerPass(new EntityDefinitionCollectionPopulatorsCompilerPass());
+        $extension = $this->getContainerExtension();
+
+        $container->addCompilerPass(new EntityDefinitionCollectionPopulatorsCompilerPass(), priority: 0);
+        $container->addCompilerPass(new MakeEntityRepositoriesAutowirableCompilerPass($extension), priority: -1);
     }
 
     public function getContainerExtension(): ITBShopwareSdkExtension
